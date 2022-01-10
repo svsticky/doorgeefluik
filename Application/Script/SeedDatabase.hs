081d6c73d8a24bb48ae0bad6c63227e5be7075d5
Hugo <hpeters1024@gmail.com>
2022-01-10 20:33:33 +0100

    Update seed script with a check if exists

--- Application/Script/SeedDatabase.hs
+++ Application/Script/SeedDatabase.hs
@@ -2,10 +2,16 @@
 module Application.Script.SeedDatabase where
 
 import Application.Script.Prelude
+import Control.Monad (void)
 
 run :: Script
 run = do
-    let route :: Route = newRecord { path = "panda", url = "https://www.youtube.com/watch?v=o-YBDTqX_ZU"}
-    route <- createRecord route
-    pure ()
+    route <- query @Route |> filterWhere (#path, "panda") |> fetchOneOrNothing
+
+    case route of
+        Just _ -> putStrLn "A recycled panda was already in place, disregard"
+        Nothing -> do
+            let route :: Route = newRecord { path = "panda", url = "https://www.youtube.com/watch?v=o-YBDTqX_ZU"}
+            void $ createRecord route
+
 
