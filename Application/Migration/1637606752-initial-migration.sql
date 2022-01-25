022e3b91540d2f919d58101d7b8025853a60d216
Hugo <hpeters1024@gmail.com>
2022-01-26 00:24:36 +0100

    enable postgres uuid extension

--- Application/Migration/1637606752-initial-migration.sql
+++ Application/Migration/1637606752-initial-migration.sql
@@ -5,6 +5,7 @@
 -- Use http://localhost:8001/NewMigration or `new-migration` to generate a new migration.
 --
 -- Learn more about migrations: https://ihp.digitallyinduced.com/Guide/database-migrations.html
+create extension if not exists "uuid-ossp";
 CREATE TABLE IF NOT EXISTS routes (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     url TEXT NOT NULL UNIQUE,
